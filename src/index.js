c728e907fa2962456a42adaafd89df76216de07d
shaswata965 <shaswata965@gmail.com>
2021-03-29 08:50:48 +0600

    Demo React

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li> Bacon </li>
+      <li> Jamon </li>
+      <li> Noodles </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
