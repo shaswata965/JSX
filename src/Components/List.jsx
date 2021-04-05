471c02b1b13f45af46f54c86c051ae3f27b71696
shaswata965 <shaswata965@gmail.com>
2021-04-05 21:04:02 +0600

    More Organised

---
+++ src/Components/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li> Bacon </li>
+      <li> Noodles </li>
+      <li> Burger </li>
+    </ul>
+  );
+}
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./Components/App";
 
-ReactDom.render(
-  <div>
-    <h1>My Favorite Foods</h1>
-    <ul>
-      <li> Bacon </li>
-      <li> Jamon </li>
-      <li> Noodles </li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDom.render(<App />, document.getElementById("root"));
